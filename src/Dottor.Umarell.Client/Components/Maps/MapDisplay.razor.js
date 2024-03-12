af9a29e8ea5be56f9bcdc382069d387ea2afcce7
Andrea Dottor <andrea@dottor.net>
2024-03-12 22:24:36 +0100

    Changed maps with leaflet

--- src/Dottor.Umarell.Client/Components/Maps/MapDisplay.razor.js
+++ src/Dottor.Umarell.Client/Components/Maps/MapDisplay.razor.js
@@ -1,49 +1,32 @@
 ﻿export async function initializeMap(container, items, callback) {
-    const { Map, InfoWindow  } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
 
-    var markers = [];
+    var map = L.map(container, { attributionControl: false })
+        .setView([45.8851534, 12.3373920], 13);
 
-    // inizializzazione mappa
-    //
-    var map = new Map(container, {
-            zoom: 18,
-            center: { lat: 45.8851534, lng: 12.3373920 },
-            disableDefaultUI: false,
-            zoomControl: true,
-            mapId: "eb895995cd6a60e5"
-        });
+    var bounds = L.latLngBounds()
 
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        //attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-    const infoWindow = new InfoWindow();
-    var bounds = new google.maps.LatLngBounds();
+    var umarellIcon = L.icon({
+        iconUrl: "/images/umarell_marker.png",
+        iconSize: [50, 100],
+        iconAnchor: [25, 99],
+        popupAnchor: [0, -95] 
+    });
 
     items.forEach(item => {
         // creazione marker
         //
-        var location = { lat: item.latitude, lng: item.longitude };
-        const markerImg = document.createElement("img");
-        markerImg.src = "/images/umarell_marker.png";
-        const glyphSvgPinElement = new PinElement({
-            glyph: markerImg,
-        });
+        var latLng = [item.latitude, item.longitude];
+        var marker = L.marker(latLng, { icon: umarellIcon });
+        marker.addTo(map);
+        bounds.extend(latLng);
 
-        let marker = new AdvancedMarkerElement({
-            position: location,
-            map: map,
-            content: glyphSvgPinElement.element,
-        });
-        markers.push(marker);
-        bounds.extend(location);
-        // gestione tooltip con le informazioni del cantiere
-        //
-        marker.addListener("click", ((id, marker, info) => {
-            return () => {
-                info.close();
-                // chiamo il metodo Blazor che si occupa di valorizzare il tooltip
-                // con le info del cantiere selezionato
-                //
-                callback.invokeMethodAsync("SetMarkerInfo", id)
+        marker.on('click', function (e) {
+             callback.invokeMethodAsync("SetMarkerInfo", item.id)
                     .then(markerInfoElement => {
                         // clono l'elemento html per non modificare/alterare quello originale
                         //
@@ -51,37 +34,29 @@
                         // lo rendo visibile
                         //
                         clonedInfo.setAttribute("style", "display: block;");
-                        info.setContent(clonedInfo);
-                        info.open(map, marker);
-                        // gestione chiusura tooltip
-                        //
-                        google.maps.event.addListener(map, 'click', () => {
-                            info.close();
-                            clonedInfo.remove();
-                        });
+                        marker.bindPopup(clonedInfo).openPopup();
 
-                        // gestione pulsanti
-                        //
-                        var buttons = clonedInfo.querySelectorAll("button");
-                        if (buttons) {
-                            buttons.forEach(button => {
 
-                                button.addEventListener("click", () => {
-                                    // notifico al codice blazor il pulsante che è stato premuto
-                                    //
-                                    callback.invokeMethodAsync("MarkerInfoButtonClick", id, button.name);
-                                });
-                            })
-                        }
+                        //// gestione pulsanti
+                        ////
+                        //var buttons = clonedInfo.querySelectorAll("button");
+                        //if (buttons) {
+                        //    buttons.forEach(button => {
+                                
+                        //        button.addEventListener("click", () => {
+                        //            // notifico al codice blazor il pulsante che è stato premuto
+                        //            //
+                        //            callback.invokeMethodAsync("MarkerInfoButtonClick", id, button.name);
+                        //        });
+                        //    })
+                        //}
 
                     });
-            }
-        })(item.id, marker, infoWindow));
 
-    })
+        });
+    });
 
-    // Center/Set Zoom of Map to cover all visible Markers
-    // https://stackoverflow.com/a/19304625/16405773
     map.fitBounds(bounds);
+
 
 }
