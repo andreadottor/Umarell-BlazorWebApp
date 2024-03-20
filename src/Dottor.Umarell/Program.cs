4b1606f7e971ab0746a9d60162f03f1c3421e337
Andrea Dottor <andrea@dottor.net>
2024-03-20 18:08:46 +0100

    Manage JSDisconnectedException

--- src/Dottor.Umarell/Program.cs
+++ src/Dottor.Umarell/Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
